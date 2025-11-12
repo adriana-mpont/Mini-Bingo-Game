322f193c51578d80299ee25e070e0aa0611be2cd
laurarigau <laura.rigau@alumni.esade.edu>
2025-11-12 09:58:34 +0100

    Sprint 4: Correction of replay feature

--- src/main.py
+++ src/main.py
@@ -9,13 +9,15 @@ def main():
         game = MiniBingo()
         # For Sprint 2: allow player to choose mode and manually advance rounds
         game.start()  # rounds handled inside display.py for manual mode selection
-
-        replay = input("\nWould you like to play another round? (Yes / No)").strip().lower()
-        if replay in ("y", "yes"): 
-            break
-        elif replay in ("n", "no"): 
-            print("The game has finished. Thank you for playing!")
-            return 
-
+        while True:
+            replay = input("\nWould you like to play another round? (Yes / No)").strip().lower()
+            if replay == "yes":
+                print("\nStarting a new game...\n")
+                break
+            elif replay == "no":
+                print("\nThe game has finished. Thank you for playing!\n")
+                return
+            else:
+                print("⚠️ Invalid input. Please type 'yes' or 'no'.")
 if __name__ == "__main__":
     main()
