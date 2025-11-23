231868d3d8a3d6424068bbd334f561bc0fff5275
annaaa-cyber <anna.scotti@alumni.esade.edu>
2025-11-23 16:22:38 +0100

    Update test_draw_marks.py

--- tests/test_draw_marks.py
+++ tests/test_draw_marks.py
@@ -3,7 +3,7 @@ from src.ui.display import MiniBingo
 #Defining a test function to verify that a drawn number that exists on the card gets marked. 
 def test_draw_marks_card():
     """Ensure a number on the card is marked."""
-    #Creating a new mini bingo game instance
+    #Creating a new mini bingo game instance.
     game = MiniBingo()
     #Picking the first number on the card as the target number to mark.
     number_to_mark = game.card.grid[0][0]
@@ -12,10 +12,10 @@ def test_draw_marks_card():
     #Failing the test if the target number has not been marked (True).
     assert game.card.marked[0][0], f"Number {number_to_mark} should be marked"
 
-#Defining a test function to ensure that invalid numbers do not mark anything
+#Defining a test function to ensure that invalid numbers do not mark anything.
 def test_mark_number_not_on_card():
     """Ensure a number not on the card is not marked."""
-    #Creating a new mini bingo game instance
+    #Creating a new mini bingo game instance.
     game = MiniBingo()
     #Picking a number outside of the valid range.
     invalid_number = 999
