463822c2b8483cef71653859c7e35035d69d24fc
annaaa-cyber <anna.scotti@alumni.esade.edu>
2025-11-23 14:51:56 +0100

    Update card.py

--- src/game/card.py
+++ src/game/card.py
@@ -10,7 +10,7 @@ class BingoCard:
         self.size = size
         #Storing the maximum number that can appear on the card.
         self.number_range = number_range
-        #Generating the bingo card grid, using the method _generate_card()
+        #Generating the bingo card grid, using the method _generate_card().
         self.grid = self._generate_card()
         #Creating a matrix of the same size as the card (4x4) to track whether each cell has been marked.
         #All cells are initialized as False.
@@ -35,13 +35,13 @@ class BingoCard:
         """Validates that the card contains 16 unique numbers within range."""
         #Retrieving all numbers from the grid in a flat list, using the method get_card_numbers().
         flat_numbers = self.get_card_numbers()
-        #Checking that all the numbers in the card are unique (no diplicates).
-        #And checking that every numebr is withing the allowed range. 
+        #Checking that all the numbers in the card are unique (no duplicates).
+        #And checking that every number is within the allowed range. 
         return len(flat_numbers) == len(set(flat_numbers)) and all(
             1 <= n <= self.number_range for n in flat_numbers
         )
 
-    #Defyning a helper method that returns all numbers in a single flat list.
+    #Defining a helper method that returns all numbers in a single flat list.
     def get_card_numbers(self):
         """Returns all numbers in a flat list."""
         #Flattening all the numbers from the matrix (grid) into a 1D list.
@@ -49,7 +49,7 @@ class BingoCard:
 
     #Defining a method to mark a number if it is present on the card.
     def mark_number(self, number: int) -> bool:
-        """Marks the number of the card if found. 
+        """Marks the number on the card if found. 
         Returns True if the number was present and marked, False otherwise. """
         #Creating a loop through each row.
         for i in range(self.size):
@@ -67,12 +67,12 @@ class BingoCard:
     #Defining a method to show the card on the screen.        
     def display_card(self):
         """Displays the Bingo card in a grid format."""
-        #Printing the title "Your Bingo Card"
+        #Printing the title "Your Bingo Card".
         print("\n Your Bingo Card")
         #Printing a separator before showing the grid.
         print("-" * (self.size * 6))
 
-        #Creating a loop over the size of the matrix (grid.
+        #Creating a loop over the size of the matrix (grid).
         for i in range(self.size):
             #Initializing a list to display each row. 
             row_display = []
@@ -80,14 +80,14 @@ class BingoCard:
             for j in range(self.size):
                 #Getting the number present in that cell.
                 num = self.grid[i][j]
-                #Checking if the number has already been marked
+                #Checking if the number has already been marked.
                 if self.marked[i][j]:
                     #If marked, storing the number inside brackets.
                     row_display.append(f"[{num:02}]")  
                 else:
                     #If not marked, storing the number without brackets.
                     row_display.append(f" {num:02} ") 
-            #Printing the completed row .
+            #Printing the completed row.
             print(" ".join(row_display))
         #Printing another separator after the grid.
         print("-" * (self.size * 6))
@@ -113,7 +113,7 @@ class BingoCard:
                 #Returning True because a new line was completed.
                 return True
 
-        #Returning False is no new row or columns has been comleted. 
+        #Returning False is no new row or columns has been completed. 
         return False
 
     #Defining a method to check if the entire card has been completed (all number marked).
@@ -121,5 +121,3 @@ class BingoCard:
         """Checks if the entire card is marked (a 'Bingo')."""
         #Checking if every cell in the grid is marked. And returning True if the entire card is completed. 
         return all(all(row) for row in self.marked)
-
-        
