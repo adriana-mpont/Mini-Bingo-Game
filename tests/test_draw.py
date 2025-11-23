ebc8ef5aded456d83b07bc71fea35b3713ee17af
annaaa-cyber <anna.scotti@alumni.esade.edu>
2025-11-23 16:21:10 +0100

    Update test_draw.py

--- tests/test_draw.py
+++ tests/test_draw.py
@@ -1,22 +1,22 @@
 import pytest
 from src.game.draw import NumberDrawer
 
-#Defining a test function to ensure that the drawn numbers are withing the valid limits. 
+#Defining a test function to ensure that the drawn numbers are within the valid limits. 
 def test_draw_returns_number_in_range():
     #Creating a new number drawer instance.
     drawer = NumberDrawer()
     #Drawing one random number.
     num = drawer.draw_number()
-    #Failing the test if the number falls outside of the rande 1 to 99.
+    #Failing the test if the number falls outside of the range 1 to 99.
     assert 1 <= num <= 99, "Drawn number out of range"
 
 #Defining a test function to ensure that no number is drawn more than once in a game. 
 def test_draw_no_repetition():
     #Creating a new number drawer instance.
     drawer = NumberDrawer()
-    #Crating a set to track the numbers already drawn. 
+    #Creating a set to track the numbers already drawn. 
     drawn_numbers = set()
-    #Crating a loop through 99 draws (which is the maximum possible). 
+    #Creating a loop through 99 draws (which is the maximum possible). 
     for _ in range(99):
         #Drawing one random number.
         num = drawer.draw_number()
@@ -39,14 +39,14 @@ def test_display_drawn_numbers(capsys):
     drawer.display_drawn_numbers()
     #Capturing printed output from display_drawn_numbers() to the console.
     captured = capsys.readouterr()
-    #Failing the test is the output does not apper as expected (with the title at the top).
+    #Failing the test is the output does not appear as expected (with the title at the top).
     assert "Numbers Drawn So Far:" in captured.out
 
 #Defining a test function to check behavior after all numbers are drawn. 
 def test_draw_after_exhaustion_returns_none():
     #Creating a new number drawer instance.
     drawer = NumberDrawer()
-    #Crating a loop through 99 draws (which is the maximum possible). 
+    #Creating a loop through 99 draws (which is the maximum possible). 
     for _ in range(99):
         #Drawing one random number.
         drawer.draw_number()
